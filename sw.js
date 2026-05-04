b19b757b09fb7da5f776c11929dcb3e1bc33d190
JobSafeCam <mehkiroberson.jobsafecam@yahoo.com>
2026-05-03 17:34:43 -0700

    Refresh PWA shell for UI fixes

--- sw.js
+++ sw.js
@@ -1,5 +1,6 @@
-const CACHE_NAME = 'jobsafecam-v2';
+const CACHE_NAME = 'jobsafecam-v3';
 const CORE_ASSETS = ['/', '/index.html', '/app.js', '/manifest.webmanifest', '/icon.svg'];
+const NETWORK_FIRST_PATHS = new Set(['/', '/index.html', '/app.js']);
 
 self.addEventListener('install', (event) => {
   event.waitUntil(
@@ -21,7 +22,7 @@ self.addEventListener('fetch', (event) => {
   if (event.request.method !== 'GET') return;
 
   const url = new URL(event.request.url);
-  if (url.origin === self.location.origin && url.pathname === '/app.js') {
+  if (url.origin === self.location.origin && NETWORK_FIRST_PATHS.has(url.pathname)) {
     event.respondWith(
       fetch(event.request)
         .then((response) => {
